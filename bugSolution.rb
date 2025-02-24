6fc43ff4e9083a5e6cdc310b29c1e68ce927ccab
BugHunter-X <askzenai@gmail.com>
2025-02-24 01:42:06 +0100

    Add bugSolution.rb file

---
+++ bugSolution.rb
@@ -0,0 +1,26 @@
+```ruby
+class MyClass
+  def initialize(value)
+    @value = value
+  end
+
+  def value
+    @value
+  end
+
+  def value=(new_value)
+    @value = new_value
+  end
+end
+
+my_object = MyClass.new(10)
+puts my_object.value # => 10
+
+# Correct way to modify the value
+my_object.value = 20
+puts my_object.value # => 20
+
+#Alternative using direct assignment
+my_object.instance_variable_set(:@value, 30)
+puts my_object.value # => 30
+```
